31199f5fcdef82afd30a63044568336fa43988b2
byi649 <byi649@aucklanduni.ac.nz>
2022-11-24 18:52:55 +1100

    Make public

---
+++ src/automail/MailPool.java
@@ -0,0 +1,107 @@
+package automail;
+
+import java.util.LinkedList;
+import java.util.Comparator;
+import java.util.ListIterator;
+
+import exceptions.ItemTooHeavyException;
+import simulation.PriorityMailItem;
+
+/**
+ * addToPool is called when there are mail items newly arrived at the building to add to the MailPool or
+ * if a robot returns with some undelivered items - these are added back to the MailPool.
+ * The data structure and algorithms used in the MailPool is your choice.
+ * 
+ */
+public class MailPool {
+
+	private class Item {
+		int priority;
+		int destination;
+		MailItem mailItem;
+		// Use stable sort to keep arrival time relative positions
+		
+		public Item(MailItem mailItem) {
+			priority = (mailItem instanceof PriorityMailItem) ? ((PriorityMailItem) mailItem).getPriorityLevel() : 1;
+			destination = mailItem.getDestFloor();
+			this.mailItem = mailItem;
+		}
+	}
+	
+	public class ItemComparator implements Comparator<Item> {
+		@Override
+		public int compare(Item i1, Item i2) {
+			int order = 0;
+			if (i1.priority < i2.priority) {
+				order = 1;
+			} else if (i1.priority > i2.priority) {
+				order = -1;
+			} else if (i1.destination < i2.destination) {
+				order = 1;
+			} else if (i1.destination > i2.destination) {
+				order = -1;
+			}
+			return order;
+		}
+	}
+	
+	private LinkedList<Item> pool;
+	private LinkedList<Robot> robots;
+
+	public MailPool(int nrobots){
+		// Start empty
+		pool = new LinkedList<Item>();
+		robots = new LinkedList<Robot>();
+	}
+
+	/**
+     * Adds an item to the mail pool
+     * @param mailItem the mail item being added.
+     */
+	public void addToPool(MailItem mailItem) {
+		Item item = new Item(mailItem);
+		pool.add(item);
+		pool.sort(new ItemComparator());
+	}
+	
+	/**
+     * load up any waiting robots with mailItems, if any.
+     */
+	public void loadItemsToRobot() throws ItemTooHeavyException {
+		//List available robots
+		ListIterator<Robot> robotIterator = robots.listIterator();
+		while (robotIterator.hasNext()) {
+			try{
+				Robot robot = robotIterator.next();
+				assert(robot.isEmpty());
+				loadItem(robot);
+				robotIterator.remove(); //Remove Robot from the queue
+				robot.dispatch(); // send the robot off if it has any items to deliver
+			} catch (Exception e) { 
+	            throw e; 
+	        } 
+		}
+	}
+	
+	//load items to the robot
+	private void loadItem(Robot robot) throws ItemTooHeavyException {
+		// System.out.printf("P: %3d%n", pool.size());
+		ListIterator<Item> j = pool.listIterator();
+		if (pool.size() > 0) {
+			robot.addToHand(j.next().mailItem); // hand first as we want higher priority delivered first
+			j.remove();
+			if (pool.size() > 0) {
+				robot.addToTube(j.next().mailItem);
+				j.remove();
+			}
+		}
+	}
+
+	/**
+     * @param robot refers to a robot which has arrived back ready for more mailItems to deliver
+     */	
+	public void registerWaiting(Robot robot) { // assumes won't be there already
+		robots.add(robot);
+	}
+
+}
